dda30c82b6fcb38bf6cad37c6c43e0d17f961607
EgiGrivandani <egigrivandani0133@gmail.com>
2026-02-19 11:01:01 +0700

    test

--- app/application/controllers/Welcome.php
+++ app/application/controllers/Welcome.php
@@ -5,25 +5,23 @@ class Welcome extends CI_Controller {
 
 	public function index()
 	{
+		$start = microtime(true);
 		$redis = new Redis();
 		$redis->connect('redis', 6379, 1.5);
-
-		$loop = 1000;
-		$start = microtime(true);
-
-		for ($i = 0; $i < $loop; $i++) {
-			$redis->set('k'.$i, 'v'.$i);
-			$redis->get('k'.$i);
-		}
-
+		echo $redis->ping('Hello World! <br>');
 		$end = microtime(true);
-
 		$totalMs = ($end - $start) * 1000;
-		$avgMs = $totalMs / ($loop * 2);
-
 		echo "Total: " . number_format($totalMs, 2) . " ms\n";
-		echo "Avg per op: " . number_format($avgMs, 4) . " ms";
-
-
 	}
+
+	public function info(){
+		echo phpinfo();
+	}
+
+	public function fpm_jebol()
+	{
+		sleep(5); // tahan 1 worker selama 20 detik
+		echo "done";
+	}
+
 }
